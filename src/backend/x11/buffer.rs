4fb4a1b0dfcbea6014daf8daa847187abf81c303
i509VCB <git@i509.me>
2021-08-14 10:25:56 -0500

    do some pixmap stuff for presenting dma or shm surfaces

--- src/backend/x11/buffer.rs
+++ src/backend/x11/buffer.rs
@@ -2,26 +2,41 @@
 //!
 //! Buffers imported into X11 are represented as X pixmaps which are then presented to the window.
 
+use std::rc::Rc;
+
 use super::X11Error;
 use x11rb::connection::Connection;
+use x11rb::protocol::xproto::ConnectionExt as _;
 use x11rb::utils::RawFdContainer;
-use x11rb::{protocol::dri3::ConnectionExt, rust_connection::RustConnection};
+use x11rb::{protocol::dri3::ConnectionExt as _, rust_connection::RustConnection};
 
+use crate::backend::allocator::Buffer;
 use crate::backend::allocator::dmabuf::Dmabuf;
 
 // Plan here is to support dmabufs via the dri3 extensions, xcb_dri3_pixmap_from_buffer.
 // Shm can also be supported easily, through xcb_shm_create_pixmap.
 
+#[derive(Debug)]
+pub struct Pixmap {
+    connection: Rc<RustConnection>,
+    inner: u32,
+}
+
+impl Drop for Pixmap {
+    fn drop(&mut self) {
+        let _ = self.connection.free_pixmap(self.inner);
+    }
+}
+
 pub fn new_dma_pixbuf(
     dmabuf: Dmabuf,
-    connection: &RustConnection,
+    connection: Rc<RustConnection>,
     window: u32,
     width: u16,
     height: u16,
     depth: u8,
     bpp: u8,
-    modifier: u64,
-) -> Result<(), X11Error> {
+) -> Result<Pixmap, X11Error> {
     // Dup FDs since XCB will close the FDs after sending them.
     // TODO:
 
@@ -42,11 +57,11 @@ pub fn new_dma_pixbuf(
             dmabuf.offsets().nth(3).unwrap(),
             depth,
             bpp,
-            modifier,
+            dmabuf.format().modifier.into(),
             // TODO: Duplicate attributes, as raw fd container takes ownership
             dmabuf.handles().map(|fd| RawFdContainer::new(fd)).collect(),
         )?
         .check()?;
 
-    Ok(())
+    Ok(todo!())
 }
